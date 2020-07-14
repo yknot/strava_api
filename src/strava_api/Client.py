2e61089156020c67974b3de6e43ca31f4bc131d7
Andrew Yale <a.yale9@gmail.com>
2020-07-13 22:02:34 -0600

    add local configuration support

--- src/strava_api/Client.py
+++ src/strava_api/Client.py
@@ -4,5 +4,12 @@
 class Client:
     """Class to manage your Strava API Client"""
 
-    def __init__(self):
-        pass
+    def __init__(
+        self, client_id: str, client_secret: str, auth_token: str, refresh_token: str
+    ) -> None:
+        """initialize client with application attributes"""
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.auth_token = auth_token
+        self.refresh_token = refresh_token
+
--- tests/test_strava_api.py
+++ tests/test_strava_api.py
@@ -1,5 +1,16 @@
+import json
 from strava_api.Client import Client
+
+config = json.load(open("config.json"))
 
 
 def test_strava_api():
-    assert Client() is not None
+    assert (
+        Client(
+            config["CLIENT_ID"],
+            config["CLIENT_SECRET"],
+            config["AUTH_TOKEN"],
+            config["REFRESH_TOKEN"],
+        )
+        is not None
+    )
